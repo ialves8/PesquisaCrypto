2fa2f6c34f2daf8735a0ab2d9067e1bff01b5490
Iranildo Ferreira Alves <ialves8@hotmail.com>
2019-04-18 10:30:59 -0300

    Add project files.

---
+++ PesquisaCrypto/Migrations/20190410203431_UpdatePropertyMoedas.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PesquisaCrypto.Migrations
+{
+    public partial class UpdatePropertyMoedas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Moedas",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Moedas",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ PesquisaCrypto/Models/Moedas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PesquisaCrypto.Models
+{
+    public class Moedas
+    {
+        public int MoedasId { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+
+        [NotMapped]
+        public bool CheckboxMarcado { get; set; }
+    }
+}
